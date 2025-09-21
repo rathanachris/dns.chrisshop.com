aa57537bfac78a77307c3937471fa9b8d87a0fa4
rathanachris <rathanachris239@gmail.com>
2025-09-21 23:08:17 +0700

    Create node.js
    Signed-off-by: rathanachris <rathanachris239@gmail.com>

---
+++ app/node.js
@@ -0,0 +1,12 @@
+const https = require('https');
+const fs = require('fs');
+const app = require('./app'); // your express app
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/dns.chrisshop.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/dns.chrisshop.com/fullchain.pem')
+};
+
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS server running!');
+});
