108317ea354669ea21d543a4b2113d218cded51f
chris rathana <rathanachris239@gmail.com>
2025-10-20 00:01:35 +0700

    Update Properties.sql

--- etc/sql.properties/init.sql/Properties.sql
+++ etc/sql.properties/init.sql/Properties.sql
@@ -1,7 +1,7 @@
 spring.datasource.url=jdbc:mysql://mysql:192.168.100.1:8080/mydb?useSSL=false&allowPublicKeyRetrieval=true
 spring.datasource.username=myuser
 spring.datasource.password=mypassword
-spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+spring.datasource.driver-class-name=com.chrisrathana.mysql.cj.jdbc.Driver
 spring.jpa.hibernate.ddl-auto=update
 spring.jpa.show-sql=true
 spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect
