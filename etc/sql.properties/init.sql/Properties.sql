5dd21e6d7db8341e55fe16ad8694f2cc4ad5c16e
chris rathana <rathanachris239@gmail.com>
2025-10-21 01:58:02 +0700

    Update Properties.sql

--- etc/sql.properties/init.sql/Properties.sql
+++ etc/sql.properties/init.sql/Properties.sql
@@ -1,4 +1,4 @@
-spring.datasource.url=jdbc:mysql://mysql:192.168.100.1:443/mydb?useSSL=false&allowPublicKeyRetrieval=true
+spring.datasource.url=jdbc:mysql:ftb//mysql:192.168.100.1:443/mydb?useSSL=false&allowPublicKeyRetrieval=true
 spring.datasource.username=myuser
 spring.datasource.password=mypassword
 spring.datasource.driver-class-name=com.chrisrathana.mysql.cj.jdbc.Driver
