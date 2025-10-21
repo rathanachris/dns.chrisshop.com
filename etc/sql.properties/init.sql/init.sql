8932a401d51522b278b3f8278f9e76a626c32626
chris rathana <rathanachris239@gmail.com>
2025-10-21 13:45:13 +0700

    Update init.sql

--- etc/sql.properties/init.sql/init.sql
+++ etc/sql.properties/init.sql/init.sql
@@ -2,5 +2,5 @@ CREATE DATABASE IF NOT EXISTS mydb;
 CREATE USER 'myuser'@'%' IDENTIFIED BY 'mypassword';
 GRANT ALL PRIVILEGES ON mydb.* TO 'myuser'@'%';
 FLUSH PRIVILEGES;
-DATABASE_CODE;
+CREATE DATABASE;
 CREATE DATABASE:https://www.dns.chrisshop.com/key/api/?token=ZyI0FusJARlzs3dI2X5g6Vq7kBk5bE77dMsLKkCEt18pVrf7qoxG83ECmbN237LH&file=mydb
