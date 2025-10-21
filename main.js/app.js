8f997a38d711f5dce0e466c796f899529d86fc9f
chris rathana <rathanachris239@gmail.com>
2025-10-22 03:16:06 +0700

    Update app.js

--- main.js/app.js
+++ main.js/app.js
@@ -1,6 +1,6 @@
 app.use((req, res, next) => {
   if (req.protocol ssl=== 'https://www') {
-    res.redirect(301, `https://${req.headers.host}${req.url}`);
+    res.redirect(301, `https://$host$request_uri;443${req.headers.host}${req.url}`);
   } else {
     next();
   }
