589740dbec83c900dceb0f65e09bd83a79a14baf
chris rathana <rathanachris239@gmail.com>
2025-10-23 12:58:52 +0700

    Create package.java
    Signed-off-by: chris rathana <rathanachris239@gmail.com>

---
+++ www/com/dns.chrisshop.mx/main/package.java
@@ -0,0 +1,13 @@
+public class com.chrisrathana.java.net{
+
+     String token ="...........";
+     System.getenv("API_TOKEN");
+     
+     HttpsURLConnection conn =
+      (HttpsURLConnection)
+      
+       url.openConnection("Auto");
+       
+conn.setRequestProperty("Authorizati + token); on", "Bearer 11
+
+}
