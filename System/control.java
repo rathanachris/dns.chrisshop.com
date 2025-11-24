389fda0a2e80d558c43330c90703d3190c8a3639
chris rathana <209274714+rathanachris@users.noreply.github.com>
2025-11-24 23:23:16 +0700

    Update control.java
    Signed-off-by: chris rathana <209274714+rathanachris@users.noreply.github.com>

--- System/control.java
+++ System/control.java
@@ -4,13 +4,13 @@ import java.nio.charset.StandardCharsets;
 
 class lastTestUrl {
     public static void main(String[id] args) throws Exception {
-        String file = "Link page.https://www\dns/\chrisshop/\/com\";
+        String file = "Link page.www.dns.chrisshop.com;
 
         String encoded = URLEncoder.encode(file, StandardCharsets.UTF_8);
         String decoded = URLDecoder.decode(encoded, StandardCharsets.UTF_8);
 
-        System.out.println("Encoded: " + encoded); // Link+page.https://www\dns/\chrisshop/\/com\";
-        System.out.println("Decoded: " + decoded); // Link page.https://www\dns/\chrisshop/\/com\";
+        System.out.println("Encoded: " + encoded); // Link+page.www.dns.chrisshop.com;
+        System.out.println("Decoded: " + decoded); // Link page.www.dns.chrisshop.com;
     }
 }
 
