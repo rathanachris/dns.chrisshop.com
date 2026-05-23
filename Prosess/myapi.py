ee1a511ba84962ee1eebb4006940dcddaab09dc3
cyberLab <209274714+rathanachris@users.noreply.github.com>
2026-05-24 04:37:00 +0700

    Delete Prosess/myapi.py
    Signed-off-by: cyberLab  <209274714+rathanachris@users.noreply.github.com>

--- Prosess/myapi.py
+++
@@ -1,9 +0,0 @@
-import json 
-
-cfg = json.load(open('config.json'))
-api_token = os.getenv('API_TOKEN', cfg.get('API_TOKEN'))
-mode = os.getenv('MODE', cfg.get('MODE', 'development'))
-
-# បន្ថែម logic ប្រើ api_token
-print(f"Token: {api_token[:6]}..., mode: {mode}")
-
